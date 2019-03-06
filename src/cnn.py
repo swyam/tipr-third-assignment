b2bcb3562bdab38e32f1f1d860fa9f9bda24bccf
Anirban Biswas <friendlyrony2050@gmail.com>
2019-03-06 20:19:55 +0530

    third assignment probkems

---
+++ src/cnn.py
@@ -0,0 +1,24 @@
+# Implement Convolutional Neural Network here!
+
+def initialize_parameters():
+	# Code for parameter initialization
+
+
+def forward_prop():
+	# Code for forward propagation
+
+
+def back_prop():
+	# Code for backward propagation
+
+
+def compute_cost():
+	# Code for cost function
+
+
+def train():
+	# Train your model
+
+
+def predict():
+	# Code for prediction
---
+++ src/main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    print('Welcome to the world of CNNs!')
+    # The entire code should be able to run from this file!
